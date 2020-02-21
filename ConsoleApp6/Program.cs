845f33ad499f1464847d3a674144829737b569a7
tgriffin3 <56778631+tgriffin3@users.noreply.github.com>
2020-02-20 18:54:29 -0600

    Add files via upload

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string blackListWord = "Trump";
+            bool isSpam = false;
+            string message = Console.ReadLine();
+
+            if (message.Contains(blackListWord))
+            {
+                isSpam = true;
+                Console.WriteLine("This is spam");
+                Console.WriteLine(blackListWord);
+            }
+            else
+            {
+                isSpam = false;
+                Console.WriteLine("This is not spam.");
+                Console.WriteLine(message);
+            }
+        }
+    }
+}
