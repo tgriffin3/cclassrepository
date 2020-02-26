c5ba5dbe92c37d9a4dbfe028dfacf0c6dbd9b330
tgriffin3 <56778631+tgriffin3@users.noreply.github.com>
2020-02-25 18:12:10 -0600

    Add files via upload

---
+++ LabSpamChecker_Tammy_Griffin/LabSpamChecker_Tammy_Griffin/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+
+namespace LabSpamChecker_Tammy_Griffin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] spamwords = { "buy", "viagra", "XXX", "free money", "lifetime offer", "send money",
+                                  "bank account", "nigeria", "online pharmacy", "h8te", "meet girls"};
+
+
+            string message = Console.ReadLine();
+            bool isSpam = false;
+            message = message.ToLower();
+            for (int i = 0; i < spamwords.Length; i++)
+            {
+                if (message.Contains(spamwords[i]))
+                {
+                    isSpam = true;
+                }
+            }
+
+            if (isSpam == true)
+            {
+                isSpam = true;
+                Console.WriteLine("This is spam");
+                Console.WriteLine(message);
+            }
+            else
+            {
+                isSpam = false;
+                Console.WriteLine("This is not spam.");
+
+            }
+        }
+    }
+}
