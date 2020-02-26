c3c2b9897e4e932d717af87b3b3a6a54ba7b872f
tgriffin3 <56778631+tgriffin3@users.noreply.github.com>
2020-02-25 18:47:37 -0600

    Add files via upload

---
+++ Lab4.6_TammyGriffin/Lab4.6_TammyGriffin/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Lab4._6_TammyGriffin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            string[] days = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday" };
+            string[] Tasks = new string[7];
+
+            for (int i = 0; i < days.Length; i++)
+            {
+
+                Console.WriteLine("Type in a new Task for:  " + days[i]);
+                Tasks[i] = days[i] + " : " + Console.ReadLine();
+
+            }
+
+            for (int i = 0; i < days.Length; i++)
+            {
+                Console.WriteLine(Tasks[i]);
+            }
+        }
+    }
+}
