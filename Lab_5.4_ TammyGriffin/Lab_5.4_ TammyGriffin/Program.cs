9cc12d540fb13041593b2ebea7a59a931e6e78ab
tgriffin3 <56778631+tgriffin3@users.noreply.github.com>
2020-02-27 17:41:31 -0600

    Add files via upload

--- Lab_5.4_ TammyGriffin/Lab_5.4_ TammyGriffin/Program.cs
+++ Lab_5.4_ TammyGriffin/Lab_5.4_ TammyGriffin/Program.cs
@@ -32,12 +32,19 @@ namespace Lab_5._4__TammyGriffin
             mario.canShootFireBalls = false;
             mario.isJumping = false;
 
+            Console.WriteLine("Mario is running through the level and meets a Gooba. Set Mario to jumping so he can kill it");
             Console.WriteLine(mario.isJumping = true);
+            Console.WriteLine("Mario squashes the Gooba. Now he needs to land!");
             Console.WriteLine(mario.isJumping = false);
+            Console.WriteLine("Mario hits a block and finds a 1UP. Increase Mario's life by one.");
             Console.WriteLine(mario.numOfLives +=1);
+            Console.WriteLine("Mario finds a secret Fire Flower and can now shoot fireballs. Change mario!");
             Console.WriteLine(mario.canShootFireBalls = true);
+            Console.WriteLine("Mario finds another power up mushroom and gets big. Change Mario");
             Console.WriteLine(mario.sizeOfMario = Mario.Size.big);
+            Console.WriteLine("Mario finds a stash of gold coins. Give Mario 10 coins.");
             Console.WriteLine(mario.numOfCoins = 10);
+            Console.WriteLine("Mario finshes the level! Change Mario's current level to World 1-2");
             Console.WriteLine(mario.currentLevel = "World 1-2");
 
         }
