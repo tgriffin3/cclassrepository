42ddb45c4de697464af2f31f193693d0d63eb6dd
tgriffin3 <56778631+tgriffin3@users.noreply.github.com>
2020-02-27 15:56:54 -0600

    Add files via upload

---
+++ Lab5.2_TammyGriffin/Lab5.2_TammyGriffin/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Lab5._2_TammyGriffin
+{
+    class Program
+    {
+        public enum WeekDays
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+        static void Main(string[] args)
+        {
+            List<string> taskList = new List<string>();
+            foreach (WeekDays day in Enum.GetValues(typeof(WeekDays)))
+            {
+                Console.WriteLine("Enter a new task for " + day);
+                string task = day + " : " + Console.ReadLine();
+                taskList.Add(task);
+            }
+            foreach (string task in taskList)
+            {
+                Console.WriteLine(task);
+            }
+
+        }
+    }
+}
